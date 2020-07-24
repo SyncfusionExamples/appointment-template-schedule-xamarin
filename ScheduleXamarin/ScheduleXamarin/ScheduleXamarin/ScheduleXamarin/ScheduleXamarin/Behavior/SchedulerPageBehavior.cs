df4f0442031115cbda2a50107e53648e8fb1f751
Jayaleshwari N <jayaleshwarin@syncfusion.com>
2020-07-24 17:02:08 +0530

    XAMARIN-34885 - Sample and screenshot added.

---
+++ ScheduleXamarin/ScheduleXamarin/ScheduleXamarin/ScheduleXamarin/ScheduleXamarin/Behavior/SchedulerPageBehavior.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using Syncfusion.SfSchedule.XForms;
+using Xamarin.Forms;
+
+namespace ScheduleXamarin
+{
+    public class SchedulerPageBehavior : Behavior<ContentPage>
+    {
+        SfSchedule schedule;
+        ContentPage ContentPage;
+        protected override void OnAttachedTo(ContentPage bindable)
+        {
+            base.OnAttachedTo(bindable);
+            this.ContentPage = bindable;
+            this.schedule = bindable.Content.FindByName<SfSchedule>("Schedule");
+            this.WireEvents();
+        }
+        
+        private void WireEvents()
+        {
+            this.schedule.CellTapped += Schedule_CellTapped;
+        }
+
+        private void Schedule_CellTapped(object sender, CellTappedEventArgs e)
+        {
+            if (e.Appointment != null)
+            {
+                var appname = (e.Appointment as Meeting).EventName;
+                var City = (e.Appointment as Meeting).City;
+                var District = (e.Appointment as Meeting).District;
+                var noOfStudents = (e.Appointment as Meeting).NumberOfStudents;
+
+                this.ContentPage.DisplayAlert(appname, "City:" + " " + City + "\n" + "District:" + " " + District + "\n" + "NumberofStudents:" + " " + noOfStudents + "\n" , "ok");
+            }
+        }
+
+        protected override void OnDetachingFrom(ContentPage bindable)
+        {
+            base.OnDetachingFrom(bindable);
+            this.UnWireEvents();
+        }
+
+        private void UnWireEvents()
+        {
+            this.schedule.CellTapped -= Schedule_CellTapped;
+        }
+    }
+}
---
+++ ScheduleXamarin/ScheduleXamarin/ScheduleXamarin/ScheduleXamarin/ScheduleXamarin/View/AppointmentDataTemplate.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Xamarin.Forms;
+
+namespace ScheduleXamarin
+{
+    public partial class AppointmentDataTemplate : AbsoluteLayout
+    {
+        public AppointmentDataTemplate()
+        {
+            InitializeComponent();
+        }
+    }
+}
